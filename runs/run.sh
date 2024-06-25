f5c5ee4f0960d2acefdf25fee2583cf97ad35076
makamoa <maksim.makarenko@kaust.edu.sa>
2024-06-25 14:26:24 +0300

    added environment files

--- runs/run.sh
+++ runs/run.sh
@@ -1,5 +1,9 @@
 cd ..
-#python train.py --num_epochs=10 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18'
-#python train.py --num_epochs=10 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18'
-python train.py --num_epochs=10 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.01
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18'
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.001
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.005
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.01
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.02
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.05
+python train.py --num_epochs=30 --batch_size=32 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.001 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.1
 #python train.py --config_file='/media/makarem/Data/complex/logs/config.yaml'
