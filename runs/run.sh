150e792bcd77c98bc6ab40cc134c6bce054d5c5d
makamoa <maksim.makarenko@kaust.edu.sa>
2024-06-27 08:10:41 +0300

    minor

--- runs/run.sh
+++ runs/run.sh
@@ -1,11 +1,60 @@
 cd ..
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18'
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.001
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.005
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.01
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.02
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.05
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.1
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.5
-python train.py --num_epochs=30 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=1.0
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.001
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.005
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.01
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.02
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.05
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.1
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=0.5
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet18' --use_sam --rho=1.0
 #python train.py --config_file='/media/makarem/Data/complex/logs/config.yaml'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.001
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.005
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.01
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.02
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.05
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.1
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=0.5
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnet50' --use_sam --rho=1.0
+#
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.001
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.005
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.01
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.02
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.05
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.1
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=0.5
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b0' --use_sam --rho=1.0
+#
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.001
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.005
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.01
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.02
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.05
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.1
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=0.5
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='efficientnet_b7' --use_sam --rho=1.0
+#
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.001
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.005
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.01
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.02
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.05
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.1
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=0.5
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest50d' --use_sam --rho=1.0
+#
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e'
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.001
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.005
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.01
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.02
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.05
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.1
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=0.5
+python train.py --num_epochs=100 --batch_size=128 --data_dir='/media/makarem/Data/cifar100' --learning_rate=0.1 --log_dir='/media/makarem/Data/unlearn/logs/' --model='resnest101e' --use_sam --rho=1.0
