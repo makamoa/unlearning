a2dea5d95cee0f71d36965f5dd3843cab09d383b
Elnur Gasanov <gasanoe@login510-27.ibex.kaust.edu.sa>
2024-07-04 11:30:50 +0300

    fixing previos commit

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -1,2 +1,3 @@
 from .sam import SAM
 from .loss import *
+from .so_sam import SO_SAM
--- optimizer/loss.py
+++ optimizer/loss.py
@@ -24,13 +24,29 @@ class NegatedKLDivLoss(nn.Module):
         kl_div_loss = self.kl_div_loss_custom(output, output_2)
         negated_kl_div_loss = -1 * kl_div_loss
         return negated_kl_div_loss
+    
+class InverseKLDivLoss(nn.Module):
+    def __init__(self, regularizer=1.0, denom_stabilizer=1e-1, temperature=1.0):
+        assert isinstance(regularizer, float), "The wrong value type"
+        assert isinstance(denom_stabilizer, float), "The wrong value type"
+        assert isinstance(temperature, float), "The wrong value type"
+        super(InverseKLDivLoss, self).__init__()
+        self.KLDivLoss = KLDivLossCustom(temperature)
+        self.regularizer = regularizer
+        self.denom_stabilizer = denom_stabilizer
+
+    def forward(self, output_1, output_2):
+        kl_loss = self.KLDivLoss(output_1, output_2)
+        return self.regularizer / (kl_loss + self.denom_stabilizer)
 
 def base_loss(model, model_teacher, input, output):
     return nn.CrossEntropyLoss()(model(input), output)
-
 
 def KL_retain_loss(model, model_teacher, input, output):
     return KLDivLossCustom()(model(input), model_teacher(input))
 
 def KL_forget_loss(model, model_teacher, input, output):
     return NegatedKLDivLoss()(model(input), model_teacher(input))
+
+def inverse_KL_forget_loss(model, model_teacher, input, output):
+    return InverseKLDivLoss(regularizer=0.1, denom_stabilizer=0.2)(model(input), model_teacher(input))
